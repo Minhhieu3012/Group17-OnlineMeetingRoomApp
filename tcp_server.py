47233bea0367021bedf835a3cd982db7e217aac0
Minhhieu3012 <hieupm9898@ut.edu.vn>
2025-08-25 18:46:25 +0700

    feature: done function main 25/8

--- tcp_server.py
+++ tcp_server.py
@@ -54,3 +54,12 @@ async def handle_client(reader,writer):
             clients.pop(username) # xóa client khỏi dict
         writer.close()
         await writer.wait_closed()
+
+async def main():
+    server = await asyncio.start_server(handle_client,'127.0.0.1',5000)
+    print("Server started on port 5000")
+    async with server:
+        await server.serve_forever()
+
+if __name__=="__main__":
+    asyncio.run(main())
