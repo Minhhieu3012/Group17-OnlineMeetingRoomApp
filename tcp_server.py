073b71ebed64d91e4fba52bf1d7bc2d45fdd97d6
Minhhieu3012 <hieupm9898@ut.edu.vn>
2025-08-25 16:51:38 +0700

    feature: done function read_msg 25/8

--- tcp_server.py
+++ tcp_server.py
@@ -5,6 +5,12 @@ import struct
 clients={} # dict để lưu trữ thông tin client
 
 async def send_msg(writer, data: dict):
-    raw=json.dumps(data).encode('utf-8')
+    raw=json.dumps(data).encode('utf-8') 
     writer.write(struct.pack('!I',len(raw))+raw) # gửi tiền tố độ dài và dữ liệu
     await writer.drain() # đảm bảo dữ liệu được gửi
+
+async def read_msg(reader):
+    header=await reader.readexactly(4) # đọc tiền tố độ dài
+    (ln,)=struct.unpack("!I",header) # giải nén độ dài
+    raw=await reader.readexactly(ln) # đọc dữ liệu
+    return json.loads(raw.decode('utf-8')) # giải mã JSON và trả về dict
