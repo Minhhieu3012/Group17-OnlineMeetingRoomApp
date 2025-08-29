7a2db0f589852276bf698e68d0f13505db67d5cc
Minhhieu3012 <hieupm9898@ut.edu.vn>
2025-08-29 15:31:55 +0700

    chore: update routing module 29/8

--- server/routing.py
+++ server/routing.py
@@ -1,21 +1,22 @@
+# hệ thống định tuyến msg trong chat server
 from tcp_state import clients, rooms
 from protocol import send_msg
 
 # Gửi trực tiếp tới 1 user
 async def send_to_user(username: str, obj: dict):
-    if username in clients:
-        await send_msg(clients[username].writer, obj)
+    if username in clients: # kiểm tra user có onl không
+        await send_msg(clients[username].writer, obj) # clients[username].writer: lấy TCP writer của user đó 
 
 # Broadcast tới phòng
 async def send_to_room(room: str, obj: dict, exclude: str = None):
-    for u in rooms.get(room, set()):
-        if u != exclude and u in clients:
+    for u in rooms.get(room, set()): # lặp qua từng username trong phòng  
+        if u != exclude and u in clients: # dk1: kh gửi cho người loại trừ / dk2: chỉ gửi cho user onl
             await send_msg(clients[u].writer, obj)
 
 # Relay tin nhắn (file, chat, ...)
 async def relay_message(sender: str, msg: dict):
-    to = msg.get("to")
-    if to:  # DM
+    to = msg.get("to") # kiểm tra loại msg 
+    if to:  # nếu có to là "DM"
         await send_to_user(to, msg)
     else:   # Broadcast trong phòng
         r = clients[sender].room
