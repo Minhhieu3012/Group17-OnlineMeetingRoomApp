d0dcc8e8bc2b1fed2fffb44dacea2506eb78eb23
Minhhieu3012 <hieupm9898@ut.edu.vn>
2025-08-29 16:25:01 +0700

    chore: update tcp_state module 29/8

--- server/tcp_state.py
+++ server/tcp_state.py
@@ -1,12 +1,13 @@
-from dataclasses import dataclass, field
-from typing import Dict, Set, Optional
+# define ctdl của server
+from dataclasses import dataclass, field # field: customize behavior cho từng field
+from typing import Dict, Set, Optional  
 import asyncio
 
 @dataclass
 class Client:
     username: str
     writer: asyncio.StreamWriter
-    room: Optional[str] = None
+    room: Optional[str] = None # = None là default value - user chưa vào phòng nào 
     udp_endpoints: dict = field(default_factory=lambda: {"audio": None, "video": None})
 
 # Global state
